ff2a3b3ee86d8217cf57e469516cb4d5d6f83a39
lambertse <lambert.software.engineer@gmaill.com>
2025-11-07 11:07:14 +0700

    Fix build failed on macOS

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 mkdir build
-cmake -DCMAKE_BUILD_TYPE=Debug -B build 
+cd build
+cmake322 .. 
 if [ $? -ne 0 ]; then
     echo "CMake configuration failed."
     exit 1
 fi
-cmake --build build 
+cmake322 --build . 
 if [ $? -ne 0 ]; then
     echo "Build failed."
     exit 1
